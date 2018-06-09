9bf3362cabeff76808cfa8438e54dcd045ad572c
Luke Horgan <horgan.l@husky.neu.edu>
2018-06-09 15:16:02 -0400

    recipe component

---
+++ src/components/Recipe.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+const recipes = [{
+  
+}];
+
+class Recipe extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  setRecipe(recipe) {
+    this.setState({recipe: recipe});
+  }
+  
+  render() {
+    return (<div>
+      This is a recipe!
+    </div>)
+  }
+}
+
+export default Recipe;
