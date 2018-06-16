3b8dedbdd40b2e6b0715eab2219ec87d9a4bc211
aliceknin <aliceknin@gmail.com>
2018-06-16 00:28:13 -0400

    modal doesn't open; libraries make me crazy

--- src/components/CreateRecipeModal.js
+++ src/components/CreateRecipeModal.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import Modal from 'react-bootstrap-modal';
 import IngredientList from './IngredientList';
 
 export default class CreateRecipeModal extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.saveRecipe = this.saveRecipe.bind(this);
+  }
+
+  saveRecipe() {
+    console.log("recipe save button clicked!");
+  }
+
   render() {
     return (
-      <div>
-        <IngredientList/>
-        <div className="form-group">
-          <label htmlFor="instructions">instructions</label>
-          <textarea className="form-control" id="instructions"
-                    placeholder="add your instructions here..."/>
-        </div>
-      </div>
+      <Modal show={this.props.show} onHide={this.props.onHide}>
+        <Modal.Header>
+          <Modal.Title id='ModalHeader'>Create a Recipe</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <IngredientList/>
+          <div className="form-group">
+            <label htmlFor="instructions">instructions</label>
+            <textarea className="form-control" id="instructions"
+                      placeholder="add your instructions here..."/>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Dismiss className="btn btn primary" onClick={this.saveRecipe}></Modal.Dismiss>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,23 +1,38 @@
 import React, {Component} from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import CreateRecipeModal from '../components/CreateRecipeModal';
 
 class Profile extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: {}
+      user: {},
+      createModalOpen: false
     };
+
+    this.openCreateModal = this.openCreateModal.bind(this);
+    this.closeCreateModal = this.closeCreateModal.bind(this);
   }
 
   componentDidMount() {
-    this.setState({user: this.props.location.state.user});
+  //  this.setState({user: this.props.location.state.user});
   }
+
+ openCreateModal() {
+   this.setState({createModalOpen: true});
+ }
+
+ closeCreateModal() {
+   this.setState({createModalOpen: false});
+ }
 
   render() {
     return (
       <div>
         <h1>{this.state.user.username}</h1>
+        <button className="btn btn-default" onClick={this.openCreateModal}>Create Recipe</button>
+        <CreateRecipeModal show={this.state.createModalOpen} onHide={this.closeCreateModal}/>
       </div>
     )
   }
