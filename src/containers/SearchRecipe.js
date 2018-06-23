bef081e51f8327e1dbd3d106871ad4a828b763f8
Luke Horgan <horgan.l@husky.neu.edu>
2018-06-23 19:43:39 -0400

    de-beautify recipe page to make it more flexible

--- src/containers/SearchRecipe.js
+++ src/containers/SearchRecipe.js
@@ -35,60 +35,73 @@ export default class Recipe extends Component {
     this.setState({tabView: 'overview'});
   }
 
+  showTab() {
+    if(this.state.tabView === "ingredients") {
+      return this.state.recipe.ingredients.map((ingredient, idx) => {
+                return <div key={idx}>
+                        {ingredient.text}
+                       </div>});
+    }
+    else if(this.state.tabView === "nutrition") {
+      if(this.state.recipe.digest) {
+        return this.state.recipe.digest.map((nutrient, idx) => {
+                  return <div key={idx}>
+                          {nutrient.label}: {nutrient.total} {nutrient.unit}
+                         </div>});
+      }
+      else {
+        return <div>This recipe has not got any nutrition info. How about you just enjoy it?</div>
+      }
+    }
+    else if(this.state.tabView === "reviews") {
+      return "Reviews will go here!";
+    }
+    else if(this.state.tabView === "overview") {
+      return <div>Overview</div>
+      /*return (<div>
+        Overview
+        <img src={this.state.recipe.image} />
+        <div>
+          <a href={this.state.recipe.url}> Click to see full recipe </a>
+        </div>
+      </div>);*/
+    }
+  }
+
   render() {
-    return (
-    <div className="row">
+    return (<div className="row">
       <div className="col-8">
         <div className="container-fluid">
-        <h1>{this.state.recipe.label}</h1>
-        <div>
-          <ul className="nav nav-tabs">
-            <li className="nav-item recipe-tab" onClick={this.overviewSelected}>
-              <div className="nav-link active">
-                Overview
-              </div>
-            </li>
-            <li className="nav-item recipe-tab" onClick={this.ingredientsSelected}>
-              <div className="nav-link active">
-                Ingredients
-              </div>
-            </li>
-            <li className="nav-item recipe-tab" onClick={this.nutrientsSelected}>
-              <div className="nav-link active">
-                Nutrition Info
-              </div>
-            </li>
-            <li className="nav-item recipe-tab" onClick={this.reviewsSelected}>
-              <div className="nav-link active">
-                Reviews
-              </div>
-            </li>
-          </ul>
-            {this.state.tabView === 'ingredients' ?
-              this.state.recipe.ingredients.map((ingredient, idx) => {
-                return <div key={idx}>
-                       {ingredient.text}
-                     </div>
-            }) :
-            this.state.tabView === 'nutrition' ?
-              this.state.recipe.digest.map((nutrient, idx) => {
-                return <div key={idx}>
-                        {nutrient.label}: {nutrient.total} {nutrient.unit}
-                       </div>
-            }) :
-            this.state.tabView === 'reviews' ?
-              <h1>Reviews will go here!!</h1>
-              : <div>
-                  <img src={this.state.recipe.image} />
-                  <div>
-                    <a href={this.state.recipe.url}> Click to see full recipe </a>
-                  </div>
+          <h1>{this.state.recipe.label}</h1>
+          <div>
+            <ul className="nav nav-tabs">
+              <li className="nav-item recipe-tab" onClick={this.overviewSelected}>
+                <div className="nav-link active">
+                  Overview
                 </div>
-              }
-        </div>
+              </li>
+              <li className="nav-item recipe-tab" onClick={this.ingredientsSelected}>
+                <div className="nav-link active">
+                  Ingredients
+                </div>
+              </li>
+              <li className="nav-item recipe-tab" onClick={this.nutrientsSelected}>
+                <div className="nav-link active">
+                  Nutrition Info
+                </div>
+              </li>
+              <li className="nav-item recipe-tab" onClick={this.reviewsSelected}>
+                <div className="nav-link active">
+                  Reviews
+                </div>
+              </li>
+            </ul>
+
+            {this.showTab()}
+
+          </div>
         </div>
       </div>
-      </div>
-    )
+    </div>)
   }
 }
